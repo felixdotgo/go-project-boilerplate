a37d2a4da8b43f6d8dd26f950f54a6cfe7c2b471
0x46656C6978 <nguyenthanhdat.js@gmail.com>
2024-09-15 11:26:14 +0000

    feat: change the way to read .env file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,60 +1,92 @@
 package config
 
 import (
-	"os"
-	"time"
-
+	"fmt"
 	"github.com/0x46656C6978/go-project-boilerplate/pkg/conv"
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
+	"strings"
 )
 
 const ENV_PRODUCTION = "production"
 
+// DB contains configurations to make a connection to RDBMS
+type DB struct {
+	Host     string `mapstructure:"db_host"`
+	User     string `mapstructure:"db_user"`
+	Password string `mapstructure:"db_password"`
+	DBName   string `mapstructure:"db_dbname"`
+	Port     string `mapstructure:"db_port"`
+}
+
+// JWT is configurations for JSON Web Token
+type JWT struct {
+	Secret string `mapstructure:"jwt_secret"`
+	Expire string `mapstructure:"jwt_expire"`
+	Issuer string `mapstructure:"jwt_issuer"`
+}
+
+// OAuth contains all configurations that related to OAuth
+type OAuth struct {
+	Google OAuthGoogle `mapstructure:",squash"`
+}
+
+// OAuthGoogle contains configs that will be used to make an auth request to Google
+type OAuthGoogle struct {
+	ClientID     string `mapstructure:"oauth_google_client_id"`
+	ClientSecret string `mapstructure:"oauth_google_client_secret"`
+	RedirectURL  string `mapstructure:"oauth_redirect_url"`
+}
+
+// Config is a struct that contains all other configurations that will be defined later
 type Config struct {
-	EnvMode string
-	Port    int
-	DB      struct {
-		Host     string
-		User     string
-		Password string
-		DBName   string
-		Port     int
-	}
-	JWT struct {
-		Secret string
-		Expire time.Duration
-		Issuer string
-	}
-	OAuth struct {
-		Google struct {
-			ClientID     string
-			ClientSecret string
-			RedirectURL	string
-		}
-	}
+	EnvMode string `mapstructure:"env_mode"`
+	Port    string `mapstructure:"port"`
+	DB      DB     `mapstructure:",squash"`
+	JWT     JWT    `mapstructure:",squash"`
+	OAuth   OAuth  `mapstructure:",squash"`
 }
 
-func New() *Config {
-	cfg := &Config{}
-	cfg.EnvMode = os.Getenv("ENV_MODE")
-	cfg.Port = conv.ToInt(os.Getenv("PORT"))
+// New returns new Config
+func New() (*Config, error) {
+	// solution: https://stackoverflow.com/a/63541140/9839165
+	v := viper.New()
+	v.AutomaticEnv()
+	v.SetConfigName(".env")
+	v.SetConfigType("env")
+	v.AddConfigPath(".")
+	v.SetEnvKeyReplacer(strings.NewReplacer(`.`, `_`))
 
-	cfg.DB.Host = os.Getenv("DB_HOST")
-	cfg.DB.User = os.Getenv("DB_USER")
-	cfg.DB.Password = os.Getenv("DB_PASSWORD")
-	cfg.DB.DBName = os.Getenv("DB_DBNAME")
-	cfg.DB.Port = conv.ToInt(os.Getenv("DB_PORT"))
+	err := v.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %s", err)
+	}
 
-	cfg.JWT.Expire = time.Duration(conv.ToInt(os.Getenv("JWT_EXPIRE"))) * time.Second
-	cfg.JWT.Secret = os.Getenv("JWT_SECRET")
-	cfg.JWT.Issuer = os.Getenv("JWT_ISSUER")
+	var result map[string]interface{}
+	var cfg *Config
 
-	cfg.OAuth.Google.ClientID = os.Getenv("OAUTH_GOOGLE_CLIENT_ID")
-	cfg.OAuth.Google.ClientSecret = os.Getenv("OAUTH_GOOGLE_CLIENT_SECRET")
-	cfg.OAuth.Google.RedirectURL = os.Getenv("OAUTH_GOOGLE_REDIRECT_URL")
+	err = v.Unmarshal(&result)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling config: %s", err)
+	}
 
-	return cfg
+	err = mapstructure.Decode(result, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding config: %s", err)
+	}
+
+	return cfg, nil
 }
 
+// IsProduction check whether env mode is production or not
 func (c *Config) IsProduction() bool {
 	return c.EnvMode == ENV_PRODUCTION
+}
+
+// GetPort return string value of Port as int
+func (c *Config) GetPort() int {
+	if c != nil {
+		return conv.ToInt(c.Port)
+	}
+	return 0
 }
