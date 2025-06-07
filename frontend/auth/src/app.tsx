f7e2b19a6a904108b546cce1ed7f6d4ec651ae3b
0x46656C6978 <nguyenthanhdat.js@gmail.com>
2025-06-07 12:39:08 +0000

    feat: install dependencies

--- frontend/auth/src/app.tsx
+++ frontend/auth/src/app.tsx
@@ -2,6 +2,8 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+
+import "@fontsource/inter";
 import "./app.css";
 
 export default function App() {
@@ -10,8 +12,6 @@ export default function App() {
       root={props => (
         <MetaProvider>
           <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
