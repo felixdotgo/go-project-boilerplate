9ff0f741febe83a9e9e58d83e21d6dc68f8ef7d5
0x46656C6978 <nguyenthanhdat.js@gmail.com>
2025-06-08 08:30:06 +0000

    feat: update default index

--- frontend/auth/src/routes/index.tsx
+++ frontend/auth/src/routes/index.tsx
@@ -1,14 +1,17 @@
-import { A } from "@solidjs/router";
+import { MetaProvider, Title } from "@solidjs/meta";
 import { ModeToggle } from "~/components/mode-toggle";
-import { Badge } from "~/components/ui/badge";
-import { Button } from "~/components/ui/button";
 
 export default function Home() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4 bg-background">
-      <Button variant="default">Button</Button>
-      <Badge variant="error">Badge</Badge>
-      <ModeToggle />
-    </main>
+    <>
+      <MetaProvider>
+        <Title>Home</Title>
+      </MetaProvider>
+      <main class="text-center mx-auto text-gray-700 p-4 bg-background">
+        <p class="text-primary">
+          Click here to change theme:  <ModeToggle />
+        </p>
+      </main>
+    </>
   );
 }
